c588d3c42204b0238b660395f3170cecf161840a
Juan Carlos Francisco Mesa <juancarlos.francisco@xnetdigital.com>
2022-04-07 17:36:09 +0100

    All tests OK

--- own_version/script.js
+++ own_version/script.js
@@ -19,24 +19,74 @@ function callback(data){
 
   let xScale;
   let yScale;
+
+  let legendXScale;
+  let legendYScale;
+
+  let minYear;
+  let maxYear;
   
   
   let width = 1200;
   let height = 600;
-  let padding = 50;
+  let padding = 70;
   
-  let svg = d3.select('svg');
+  let svg = d3.select('#svg');
+
+  let legend = d3.select('#legend');
+
+  let tooltip = d3.select('#tooltip');
+
+  let monthName = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "Jun",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
   
   svg
     .attr('width',width)
     .attr('height',height);
+
+  legend
+    .attr('width', width / 2)
+    .attr('height', height /10);
+
             
   let generateScales = () => {
+
+    minYear = d3.min(values, (item) => {
+      return item.year;
+    });
+
+    maxYear = d3.max(values, (item) => {
+      return item.year;
+    });
+
+
     xScale = d3.scaleLinear()
+            .domain([minYear, maxYear + 1])
             .range([padding, width - padding]);
 
     yScale = d3.scaleTime()
+            .domain([new Date(0,0,0,0,0,0,0),new Date(0,12,0,0,0,0,0)])
             .range([padding, height - padding]);
+
+    legendXScale = d3.scaleLinear()
+            .domain([-3,3])
+            .range([padding / 4, width / 2 - padding / 4]);
+
+    legendYScale = d3.scaleLinear()
+            .domain([0,1])
+            .range([20,40]);
 
   };
   
@@ -55,18 +105,73 @@ function callback(data){
         if (item.variance <= -1) {
           return 'darkblue';
         }else if (item.variance <= 0) {
-          return 'blue';
+          return 'lightblue';
         }else if (item.variance <= 1) {
           return 'yellow';
         }
         return 'red';
+      })
+      .attr('height',(height - (2 * padding))/12)
+      .attr('y', (item) => {
+        return yScale(new Date(0,(item.month)-1),0,0,0,0,0);
+      })
+      .attr('width', (width - (2*padding))/(maxYear - minYear))
+      .attr('x', (item) => {
+        return xScale(item.year);
+      })
+      .on('mouseover',(event, item)=>{
+        tooltip
+          .transition()
+          .duration(500)
+          .style('visibility','visible');
+        tooltip
+          .attr('data-year', item.year);
+        tooltip
+          .text(item.year + '-' + monthName[item.month - 1]);
+      })
+      .on('mouseout',(event, item)=>{
+        tooltip
+          .transition()
+          .duration(500)
+          .style('visibility','hidden');
+      })
+      
+      ;
+
+    legend
+      .selectAll('rect')
+      .data([-3,-2,-1,0,1,2,3])
+      .enter()
+      .append('rect')
+      .attr('fill', (item) => {
+        if (item < -1) {
+          return 'darkblue';
+        }else if (item < 0) {
+          return 'lightblue';
+        }else if (item < 1) {
+          return 'yellow';
+        }
+        return 'red';
+      })
+      .attr('height',(item) => {
+        return legendYScale(0.5);
+      })
+      .attr('y',20)
+      .attr('width',(width / 2 - padding / 4)/6)
+      .attr('x',(item) => {
+        return legendXScale(item);
       });
+
   
   };
   
   let generateAxes = () => {
-    let xAxis = d3.axisBottom(xScale);
-    let yAxis = d3.axisLeft(yScale);
+    let xAxis = d3.axisBottom(xScale)
+                .tickFormat(d3.format('d'));
+    let yAxis = d3.axisLeft(yScale)
+                .tickFormat(d3.timeFormat('%B'));
+    let legendXAxis = d3.axisBottom(legendXScale)
+                .ticks(6);
 
     svg
       .append('g')
@@ -79,6 +184,12 @@ function callback(data){
       .call(yAxis)
       .attr('id','y-axis')
       .attr('transform', 'translate(' + padding + ',0)');
+
+    legend
+      .append('g')
+      .call(legendXAxis)
+      .attr('id','legendXAxis')
+      .attr('transform','translate(0,' +  40 + ')' );
   };
 
   generateScales();
